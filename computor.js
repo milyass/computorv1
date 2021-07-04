aeba40b2fd518c699f05c4c12abf85ca7c47c137
milyass <m.ilyassdev@gmail.com>
2021-07-04 21:41:07 +0100

    Parser, 1 & 0 Deg Solvers, Main Func

--- computor.js
+++ computor.js
@@ -1,7 +1,43 @@
-const polynomials = require("./lib/polynomials")
+const process = require("process");
+const polynomials = require("./lib/polynomials");
+const { output, chalk } = require("./lib/output");
+const input = process.argv[2];
 
-// Reduced Form
-// Polynomial degree
+computor_v1 = (polynomial) => {
+  try {
+    if (!polynomial) throw new Error("no input.");
+    const parsed = polynomials.parse(polynomial);
+    if(!parsed) throw new Error("not parsed.")
+    const {degree, reduced_form, a, b, c } = parsed
+    if(reduced_form === undefined || degree === undefined) throw new Error("wrong format")
+    output(chalk`Reduced form: {green ${reduced_form}}`, "bold");
+    output(chalk`Polynomial degree: {yellow ${degree}}`, "bold");
+    switch (true) {
+      case degree === 0:
+        polynomials.zero_degree(c)
+        break;
+      case degree === 1:
+        polynomials.first_degree(c, b)
+        break;
+      case degree === 2:
+        polynomials.second_degree(a, b, c)
+        break;
+      default:
+        output(
+          chalk`{cyan The polynomial degree is strictly greater than 2, I can't solve...}`,
+          "italic"
+        );
+        break;
+    }
 
-polynomials.second_degree(10, 10 , 10)
-// polynomials.first_degree(5)
+  } catch (error) {
+    output(chalk`{red ‼️ debug: ${error.message}}`, "bold");
+    output(
+      chalk`{green Computor V1} - node computor.js {cyan <Polynomial>}`,
+      "italic"
+    );
+    process.exit(0);
+  }
+};
+
+computor_v1(input);
